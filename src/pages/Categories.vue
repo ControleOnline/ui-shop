d30576653f876fd7db613e34a2343106761d4b2a
Luiz Kim <luiz.kim@controleonline.com>
2024-09-25 13:27:12 -0300

    Update

--- src/pages/Categories.vue
+++ src/pages/Categories.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="q-pa-md row">
-
-    <div class="col-md-3 col-sm-4 col-6 q-pa-sm" v-for="categorie in categories" :key="categorie.id" >
-
-      <router-link exact v-bind:to="'/shop/category/' + categorie.id ">
-
+    <div
+      class="col-md-3 col-sm-4 col-6 q-pa-sm"
+      v-for="categorie in categories"
+      :key="categorie.id"
+    >
+      <router-link exact v-bind:to="'/shop/category/' + categorie.id">
         <q-card v-ripple class="cursor-pointer">
-          <img src="https://cdn.quasar.dev/img/mountains.jpg">
+          <img src="https://cdn.quasar.dev/img/mountains.jpg" />
 
           <q-card-section>
             <div class="text-h6">{{ categorie["@type"] }}</div>
@@ -16,12 +17,8 @@
             {{ categorie["@type"] }}
           </q-card-section>
         </q-card>
-
       </router-link>
-
-
     </div>
-
   </div>
 </template>
 
@@ -42,29 +39,21 @@ export default {
       getCategories: "shop/getProductCategories",
     }),
 
-    categorias(){
-
+    categorias() {
       this.getCategories()
-      .then((response) => {
+        .then((response) => {
           this.categories = response;
-          console.log(this.categories);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
+        })
+        .catch((error) => {});
     },
   },
 
-  data(){
-    return{
-        categories: []
-    }
-  }
-
-}
+  data() {
+    return {
+      categories: [],
+    };
+  },
+};
 </script>
 
-<style lang="sass" scoped>
-
-</style>
+<style lang="sass" scoped></style>
