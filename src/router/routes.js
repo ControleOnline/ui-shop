c3a86302a6afaa6460ba136943f7eddb2283f7a4
Luiz Kim <luiz.kim@controleonline.com>
2025-03-07 12:31:24 -0300

    Update

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,40 +1,41 @@
 export const routes = [
   {
     path: "/shop",
-
     component: () =>
       import("@controleonline/ui-layout/src/layouts/ShopLayout.vue"),
     children: [
       {
         name: "ShopSearch",
         path: "q=:q",
+        meta: { isPublic: true },
         component: () => import("../pages/Search.vue"),
       },
       {
         name: "ShopDefault",
         path: "",
+        meta: { isPublic: true },
         component: () => import("../pages/Home.vue"),
       },
       {
         name: "ShopCategories",
         path: "categories",
+        meta: { isPublic: true },
         component: () => import("../pages/Categories.vue"),
       },
 
       {
         name: "ProductsInCategory",
         path: "category/:id",
+        meta: { isPublic: true },
         component: () => import("../pages/Category.vue"),
       },
       {
         name: "AllProducts",
         path: "products",
+        meta: { isPublic: true },
         component: () => import("../pages/Category.vue"),
       },
     ],
-    meta: {
-      requiresAuth: false,
-    },
   },
 
   {
@@ -45,6 +46,7 @@ export const routes = [
       {
         name: "ShopProductDetails",
         path: ":id/details",
+        meta: { isPublic: true },
         component: () => import("../pages/Product.vue"),
       },
     ],
