477d43b845d3fde6a5c7950ec63ea11a70636e02
Luiz Kim <luiz.kim@controleonline.com>
2025-03-16 14:06:00 -0300

    Update

--- src/react/styles/global.js
+++ src/react/styles/global.js
@@ -1,24 +1,37 @@
-const { StyleSheet } = require("react-native");
+const {StyleSheet} = require('react-native');
+import {getStore} from '@store';
 
-export default globalStyles = StyleSheet.create({
+export default css = () => {
+  const {getters} = getStore('theme');
+  const {colors} = getters;
+  const globalStyles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#f4f4f4',
-        paddingTop: 20,
-        paddingHorizontal: 20,
+      flex: 1,
+      backgroundColor: '#f4f4f4',
+      paddingTop: 20,
+      paddingHorizontal: 20,
     },
     loadingContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
     },
     button: {
-        padding: 11,
-        justifyContent: 'center',
-        alignItems: 'center',
+      padding: 11,
+      justifyContent: 'center',
+      alignItems: 'center',
     },
     primary: {
-        backgroundColor: '#40b8af',
-        color: '#000000',
-    }
-});
+      backgroundColor: colors['primary'],
+      color: '#000000',
+    },
+    btnAdd: {
+      flex: 1,
+      color: '#fff',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: colors['primary'],
+    },
+  });
+  return globalStyles;
+};
