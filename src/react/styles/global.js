5c9ca02fec0b8cac1718d9ea2161f9465bcc735f
Luiz Kim <luiz.kim@controleonline.com>
2025-03-17 00:11:11 -0300

    Update

--- src/react/styles/global.js
+++ src/react/styles/global.js
@@ -31,6 +31,7 @@ export default css = () => {
       justifyContent: 'center',
       alignItems: 'center',
       backgroundColor: colors['primary'],
+      flexDirection: 'row',
     },
 
     state: {
