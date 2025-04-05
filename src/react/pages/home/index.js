923777be04e9e75be1511527f75a3d9232b72dbb
Luiz Kim <luiz.kim@controleonline.com>
2025-04-05 20:36:17 -0300

    Update

--- src/react/pages/home/index.js
+++ src/react/pages/home/index.js
@@ -56,6 +56,20 @@ export default function HomePage({navigation}) {
       backgroundColor: '#4682b4',
       onPress: () => handleTo('CashRegisterIndex'),
     },
+    {
+      id: '3',
+      title: 'Estoque',
+      icon: 'archive',
+      backgroundColor: '#b48c46',
+      onPress: () => handleTo('CashRegisterIndex'),
+    },
+    {
+      id: '4',
+      title: 'Compras',
+      icon: 'shopping-bag',
+      backgroundColor: '#4ca96b',
+      onPress: () => handleTo('PurchasingSuggestion'),
+    },
   ];
 
   const renderButton = ({item}) => (
