02b88f0c3385dc0dac016f8eadc29991c7e82a72
Luiz Kim <luizkim@users.noreply.github.com>
2026-04-04 16:26:10 -0300

    PDV

--- src/react/components/ShopToolbar.js
+++ src/react/components/ShopToolbar.js
@@ -1,6 +1,5 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
-import { useNavigation, useFocusEffect } from '@react-navigation/native';
 
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigationState } from '@react-navigation/native';
@@ -9,66 +8,15 @@ import { useStore } from '@store';
 const ShopToolbar = ({ navigation }) => {
   const state = useNavigationState(state => state);
   const activeTab = state.routes[state.index]?.name || 'HomePage';
-  const currentPageName =
-    navigation.getState().routes[navigation.getState().index].name;
   const device_configStore = useStore('device_config');
   const deviceConfigGetters = device_configStore.getters;
   const { item: device } = deviceConfigGetters;
-  const authStore = useStore('auth');
-  const authGetters = authStore.getters;
-  const authActions = authStore.actions;
   const peopleStore = useStore('people');
   const peopleGetters = peopleStore.getters;
   const themeStore = useStore('theme');
   const getters = themeStore.getters;
-  const { isLogged } = authGetters;
   const { colors } = getters;
   const { currentCompany } = peopleGetters;
-  const [posType, setPosType] = useState(null);
-  const deviceStore = useStore('device');
-  const deviceGetters = deviceStore.getters;
-  const { item: storagedDevice } = deviceGetters;
-
-  useFocusEffect(
-    useCallback(() => {
-      if (storagedDevice && isLogged && currentPageName != 'SettingsPage') {
-        if (
-          device &&
-          device?.configs &&
-          Object.entries(device.configs).length > 0 &&
-          // ALEMAC // pega o appVersion ao invés do buildNumber
-          device.configs['config-version'] == storagedDevice.appVersion
-        ) {
-          setPosType(device.configs['pos-type'] || 'full');
-        } else {
-          navigation.reset({
-            index: 0,
-            routes: [{ name: 'SettingsPage' }],
-          });
-        }
-      }
-    }, [device, storagedDevice, isLogged]),
-  );
-
-  useFocusEffect(
-    useCallback(() => {
-      if (
-        device &&
-        device?.configs &&
-        Object.entries(device.configs).length > 0 &&
-        (device.configs['cash-wallet-closed-id'] == undefined ||
-          device.configs['cash-wallet-closed-id'] > 0) &&
-        isLogged &&
-        currentPageName != 'CloseCashRegister' &&
-        currentPageName != 'SettingsPage'
-      ) {
-        navigation.reset({
-          index: 0,
-          routes: [{ name: 'CloseCashRegister' }],
-        });
-      }
-    }, [device, storagedDevice, isLogged]),
-  );
 
   const styles = StyleSheet.create({
     toolbar: {
